9f13c8fb123885fce19f85ee1b3225b1ba2d8466
jahirsheikh <jahirsheikh14@gmail.com>
2024-02-28 10:59:30 +0600

    Initial commit

---
+++ src/main/java/com/jwtsecurtiy/jwt_token_security/JwtTokenSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jwtsecurtiy.jwt_token_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwtsecurtiy/jwt_token_security/JwtTokenSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtsecurtiy.jwt_token_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
